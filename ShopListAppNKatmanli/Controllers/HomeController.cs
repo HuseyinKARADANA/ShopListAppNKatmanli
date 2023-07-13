8ff8e88ee8ceae223a7bb8d05d9e32ac8e2063c0
Hüseyin Karadana <huseyinkaradana35@gmail.com>
2023-07-13 12:59:16 +0300

    Add project files.

---
+++ ShopListAppNKatmanli/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopListAppNKatmanli.Models;
+using System.Diagnostics;
+
+namespace ShopListAppNKatmanli.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
