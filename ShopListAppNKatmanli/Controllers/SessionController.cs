f849b384f12bd878d0e16cb39839f77aeb166453
Hüseyin Karadana <huseyinkaradana35@gmail.com>
2023-07-13 15:06:05 +0300

    N Katman Yapıldı Kodlar Eklendi ama kayıt işlemi gerçekleşmiyor

--- ShopListAppNKatmanli/Controllers/SessionController.cs
+++ ShopListAppNKatmanli/Controllers/SessionController.cs
@@ -4,28 +4,51 @@ using Microsoft.EntityFrameworkCore;
 using DataAccessLayer;
 using System.Data.SqlClient;
 using DataAccessLayer.Abstract;
+using ShopListAppNKatmanli.Models;
+using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using BusinessLayer.Abstract;
 
 namespace ShoppingListApp.Mapping
 {
     public class SessionController : Controller
     {
-        
+        private readonly IUserDal _userService;
 
         [HttpGet]
         public ActionResult Register()
         {
             ViewBag.register = "register";
             ViewBag.registerActive = "active";
-            return View();
+            return View(new UserAddViewModel());
         }
 
         [HttpPost]
-        public ActionResult Register(IUserDal user )
+        public ActionResult Register(UserAddViewModel model)
         {
-            
-            return View();
+            if (ModelState.IsValid)
+            {
+                _userService.Insert(new User()
+                {
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    Email = model.Email,
+                    Gender = model.Gender,
+                    BirthDate = model.BirthDate,
+                    PhoneNumber = model.PhoneNumber,
+                    RegisterDate = System.DateTime.Now,
+                    UserName = model.UserName,
+                    Password = model.Password,
+                });
+
+                return RedirectToAction("Index", "Home");
+
+
+            }
+            ViewBag.register = "register";
+            ViewBag.registerActive = "active";
+            return View(model);
+
         }
-
-
     }
 }
--- ShopListAppNKatmanli/Program.cs
+++ ShopListAppNKatmanli/Program.cs
@@ -1,40 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
-using System;
 using DataAccessLayer.Contexts;
+using Microsoft.AspNetCore.Http;
 
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();//
-
-builder.Services.AddDbContext<AppDbContext>(options =>// dataaccesslayer ile appdbcontext gelecek
+namespace ShoppingListApp
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
-});
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());//
-var app = builder.Build();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+            });
+
+            builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
